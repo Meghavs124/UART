dfa021e4f47f70d26ae01438172d0acee50bfaac
Meghavs124 <meghavs777@gmail.com>
2026-05-19 16:07:57 +0530

    uart_rx

--- source/design/uart_rx.v
+++ source/design/uart_rx.v
@@ -1,4 +1,3 @@
-
 module uart_rx #(parameter width = 8)(
     input                   sys_rst,
     input                   baud_op_clk,
@@ -6,110 +5,94 @@ module uart_rx #(parameter width = 8)(
     output reg              rec_busy,
     output reg              rec_ready,
     output reg [width-1:0]  rec_data_h
+    
 );
-
     localparam idle  = 2'd0,
                start = 2'd1,
                data  = 2'd2,
                stop  = 2'd3;
 
     reg [1:0]             ct, nt;
-    reg [3:0]             count;
     reg [$clog2(width):0] index;
     reg                   rx1, rx2;
-
+    reg                   rx2_sampled;
+    reg [3:0]        count;
+    
     always @(posedge baud_op_clk or negedge sys_rst) begin
-        if (!sys_rst) begin
-            rx1 <= 1'b1;
-            rx2 <= 1'b1;
-        end
-        else begin
-            rx1 <= uart_rec_data_h;
-            rx2 <= rx1;
-        end
+        if (!sys_rst) begin rx1 <= 1'b1; rx2 <= 1'b1; end
+        else          begin rx1 <= uart_rec_data_h; rx2 <= rx1; end
     end
 
-
     always @(posedge baud_op_clk or negedge sys_rst) begin
         if (!sys_rst) begin
-            ct         <= idle;
-            count      <= 0;
-            index      <= 0;
-            rec_data_h <= 0;
-            rec_ready  <= 0;
-            rec_busy   <= 0;
+            ct          <= idle;
+            count       <= 0;
+            index       <= 0;
+            rec_data_h  <= 0;
+            rec_ready   <= 0;
+            rec_busy    <= 0;
+            rx2_sampled <= 1;
         end
         else begin
             ct <= nt;
 
-            if (ct != nt)
-                count <= 0;
-            else
-                count <= count + 1;
+            if (ct != nt) count <= 0;
+            else          count <= count + 1;
 
-            if (ct == data && count == 7)
-                rec_data_h <= {rx2, rec_data_h[width-1:1]};
+            
+            if (ct == data && count == 5)
+                rx2_sampled <= rx2;
 
+           
+            if (ct == data && count == 6)
+                rec_data_h <= {rx2_sampled, rec_data_h[width-1:1]};
+
+            
             if (ct == idle)
                 index <= 0;
             else if (ct == data && count == 15)
                 index <= index + 1;
 
-            // Stop bit check
+           
             if (ct == stop && count == 15 && rx2 != 1'b1)
                 rec_data_h <= 0;
 
+            
+            if (ct == stop && count == 15 && rx2 == 1'b1)
+                rec_ready <= 1;
+            else
+                rec_ready <= 0;
 
-            rec_ready <= (ct == stop && nt == idle);  // 1-cycle pulse
-            rec_busy  <= (nt != idle);
+            rec_busy <= (nt != idle);
         end
     end
 
     always @(*) begin
         nt = ct;
-
         case (ct)
-
-            idle: begin
-                if (rx2 == 1'b0)
-                    nt = start;
-                else
-                    nt = idle;
-            end
-
-       start: begin
-    if (count == 15) begin  
-        if (rx2 == 0)
-            nt = data;
-        else
-            nt = idle;
-    end
-    else
-        nt = start;       
-end
-         
-           data: begin
-    if (count == 15) begin   
-        if (index == width - 1)
-            nt = stop;
-        else
-            nt = data;
-    end
-    else
-        nt = data;
-end
+            
+            idle: nt = (uart_rec_data_h == 1'b0) ? start : idle;
 
             
-            stop: begin
-                if (count == 15)
-                    nt = idle;
+            start: begin
+                if (count == 6 && rx2 != 1'b0)
+                    nt = idle;          
+                else if (count == 15)
+                    nt = data;          
                 else
-                    nt = stop;
+                    nt = start;
             end
 
-            default: nt = idle;
+            data: begin
+                if (count == 15)
+                    nt = (index == width - 1) ? stop : data;
+                else
+                    nt = data;
+            end
 
+            stop: nt = (count == 15) ? idle : stop;
+
+            default: nt = idle;
         endcase
     end
-
 endmodule
