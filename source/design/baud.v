675539b8f306ae9d5676d752c7e6a60eddc86967
Meghavs124 <meghavs777@gmail.com>
2026-05-21 11:17:31 +0530

    baud

--- source/design/baud.v
+++ source/design/baud.v
@@ -2,21 +2,23 @@ module baud_rate #(parameter freq=50000000,baudr=2400)(
     input sys_clk,
     input sys_rst,
     output reg uart_clk
-    );
-    integer endcount=(freq/(baudr*16*2));
-    reg [30:0] count=0;
+);
+
+    reg count;
+
     always @(posedge sys_clk or negedge sys_rst)
-        begin   
-            if(!sys_rst) 
-				begin 
-					uart_clk <=0;
-					count<=0; 
-				end
-            else if(count == endcount-1) 
-				begin 
-					uart_clk <= ~uart_clk;
-					count<=0;end
-            else 
-				count <= count+1;
-        end       
+    begin
+        if(!sys_rst) begin
+            uart_clk <= 0;
+            count <= 0;
+        end
+        else if(count == 1) begin
+            uart_clk <= ~uart_clk;
+            count <= 0;
+        end
+        else begin
+            count <= count + 1;
+        end
+    end
+
 endmodule
